53e32f1f8a7733ac66886384f656c3d9ecac7cc7
Robert Günzler <r@gnzler.io>
2019-09-27 11:45:15 +0200

    Fix pgup/pgdown position to be in line with up/down
    Signed-off-by: Robert Günzler <r@gnzler.io>

--- keyboards/planck/keymaps/r/keymap.c
+++ keyboards/planck/keymaps/r/keymap.c
@@ -99,7 +99,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT_planck_grid(
     _______, KC_1,  KC_2,   KC_3,   KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
     _______, KC_F1, KC_F2,  KC_F3,  KC_F4,   XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, XXXXXXX,
-    _______, KC_F5, KC_F6,  KC_F7,  KC_F8,   XXXXXXX, XXXXXXX, XXXXXXX, KC_PGDN, KC_PGUP, XXXXXXX, KC_MUTE,
+    _______, KC_F5, KC_F6,  KC_F7,  KC_F8,   XXXXXXX, XXXXXXX, KC_PGDN, KC_PGUP, XXXXXXX, XXXXXXX, KC_MUTE,
     _______, KC_F9, KC_F10, KC_F11, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
 ),
 
